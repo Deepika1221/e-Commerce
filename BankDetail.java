6a0f4838f413815ad9a2e9d0abce8d982dc9ad16
Deepika Maheshwari <dmaheshwari33@yahoo.in>
2018-08-28 23:57:26 +0530

    Add files via upload

---
+++ BankDetail.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class BankAccount
+{
+	{
+		 String Name;
+	   int AccNum;
+	   String AccType;
+	   double AccBal;
+	   double InitialBal;
+	}
+ 
+ String Customer(String N)
+ {
+ 	
+ 	return(Name);
+ }
+ 
+ double initialBal(double x)
+  {
+  	return(InitialBal);
+  }
+ double depAmt(double x)
+ {
+ 	double depAmt = y;
+ 	InitialBal = x;
+ 	AccBal = x+y;
+ 	return (AccBal);
+ 	
+ } 
+ double withdrawAmt(double x)
+ {
+ 	double withdrawAmt = y;
+ 	InitialBal = x;
+ 	AccBal = x- y;
+ 	return(AccBal);
+ 	
+ }
+  }
+ 
+ class BankDetail
+ {
+ 	public static void main (String args [])
+ 	{
+ 		BankAccount B1 = new BankAccount();
+ 		B1.Name(AbC);
+ 		initialBal1 = B1.initialBal(3000.10);
+ 		AccBal1 = B1.depAmt(200.20);
+ 		AccBal2 = B1.withdrawAmt(10.20);
+ 		System.out.println("Balance after deposit "+ AccBAl1);
+ 		System.out.println("Balance after withdrawal "+ AccBal2);
+ 		
+ 	}
+ }
+ 
